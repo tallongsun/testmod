3b6abe794e948b74a1534215e5417bbaeac485b8
sun.xin <sun.xin@linecorp.com>
2019-03-01 11:00:18 +0800

    Change Hi to allow multilang

--- testmode.go
+++ testmode.go
@@ -1,6 +1,9 @@
 package testmod
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 
 //go mod init github.com/tallongsun/testmod
@@ -14,6 +17,24 @@ import "fmt"
 //git tag v1.0.0
 //git push --tags
 
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s!",name)
+//git checkout -b v1
+//git push -u origin v1
+//git commit -m "Emphasize our friendliness" testmod.go
+//git tag v1.0.1
+//git push --tags origin v1
+
+
+func Hi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "pt":
+		return fmt.Sprintf("Oi, %s!", name), nil
+	case "es":
+		return fmt.Sprintf("¡Hola, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", errors.New("unknown language")
+	}
 }
