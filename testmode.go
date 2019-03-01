ebf92a897abc4364deef9dfb0e24dcc3fd54f5b5
sun.xin <sun.xin@linecorp.com>
2019-03-01 10:47:03 +0800

    emphasize our friendliness

--- testmode.go
+++ testmode.go
@@ -5,6 +5,15 @@ import "fmt"
 
 //go mod init github.com/tallongsun/testmod
 
+//git init
+//git add .
+//git commit -m "first commit"
+//git remote add origin https://github.com/tallongsun/testmod.git
+//git push -u origin master
+
+//git tag v1.0.0
+//git push --tags
+
 func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s",name)
+	return fmt.Sprintf("Hi, %s!",name)
 }
